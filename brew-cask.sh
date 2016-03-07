a1ea975c0b7f621369c486d73de2a19882e43848
Michael Hume <catbeer@74.opi-classless.84.135.150.in-addr.arpa>
2016-03-07 13:44:34 -0700

    Refactor brew and add zsh-setup

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,27 +1,9 @@
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
-brew cask install google-chrome
-brew cask install google-drive
-brew cask install firefox
-brew cask install microsoft-lync
-brew cask install microsoft-office
-brew cask install caffeine
-brew cask install karabiner
-brew cask install robomongo
-brew cask install atom
-brew cask install whatpulse
+brew cask install google-chrome google-drive firefox caffeine karabiner robomongo atom whatpulse
 brew tap caskroom/fonts
 brew cask install font-source-code-pro
-brew cask install keyboard-cleaner
-
-brew cask install spotify
-brew cask install viscosity
-brew cask install vlc
-brew cask install appcleaner
-brew cask install skype
-brew cask install slack
-brew cask install sequel-pro
-brew cask install skitch
+brew cask install spotify viscosity vlc appcleaner skype slack sequel-pro skitch keyboard-cleaner
 
 
