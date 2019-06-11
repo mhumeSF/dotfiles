3e49f481441ae25f89f756b9cd7aeb77ec0a82ae
Michael Hume <mhume@goodrx.com>
2019-06-11 16:34:58 -0700

    For loops

--- brew-cask.sh
+++ brew-cask.sh
@@ -5,20 +5,26 @@ brew tap caskroom/fonts
 
 brew install brew-cask-completion
 
-brew cask install appcleaner
-brew cask install backblaze
-brew cask install caffeine
-brew cask install docker
-brew cask install dropbox
-brew cask install font-source-code-pro
-brew cask install google-chrome
-brew cask install keyboard-cleaner
-brew cask install selfcontrol
-brew cask install sequel-pro
-brew cask install skitch
-brew cask install slack
-brew cask install spotify
-brew cask install viscosity
-brew cask install vlc
-brew cask install wavebox
+apps=(
+    appcleaner
+    backblaze
+    caffeine
+    docker
+    dropbox
+    font-source-code-pro
+    google-chrome
+    keyboard-cleaner
+    selfcontrol
+    sequel-pro
+    skitch
+    slack
+    spotify
+    viscosity
+    vlc
+    wavebox
+)
 
+for app in ${apps[@]}
+do
+    brew cask install $app
+done
