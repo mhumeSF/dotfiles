b6537c541bf16f0f040f6d9a24169e2fa893c7ec
Michael Hume <mhume@goodrx.com>
2019-06-11 16:26:16 -0700

    Moar updates

--- brew.sh
+++ brew.sh
@@ -6,44 +6,35 @@ brew update
 # Upgrade any already-installed formulae
 brew upgrade
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # GNU core utilities (those that come with OS X are outdated)
+#
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
 brew install moreutils
 # GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
 brew install findutils
 # GNU `sed`, overwriting the built-in `sed`
-brew install gnu-sed --default-names
-
-# Bash 4
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
-brew install bash
-
-brew install bash-completion
-brew install brew-cask-completion
-
-# generic colouriser  http://kassiopeia.juls.savba.sk/~garabik/software/grc/
-#brew install grc
-
-# Install wget with IRI support
-brew install wget --enable-iri
-
+brew install gnu-sed
 # Install more recent versions of some OS X tools
-brew install vim --override-system-vi
 brew install grep
-echo "# PATH for GNUGrep"
-echo 'PATH="/Users/mhume/.homebrew/opt/grep/libexec/gnubin:$PATH"' >> ~/.zshrc
+echo 'PATH="$(brew --prefix golang)/libexec/gnubin:$PATH"' >> ~/.zshrc
 
-# mtr - ping & traceroute. best.
-brew install mtr
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# HAVEN'T NEEDED MTR IN PAST TWO YEARS
+#
+# # mtr - ping & traceroute. best.
+# brew install mtr
+#
+# # allow mtr to run without sudo
+# mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/paulirish/.homebrew/Cellar/mtr/0.86`
+# sudo chmod 4755 $mtrlocation/sbin/mtr
+# sudo chown root $mtrlocation/sbin/mtr
 
-# allow mtr to run without sudo
-mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/paulirish/.homebrew/Cellar/mtr/0.86`
-sudo chmod 4755 $mtrlocation/sbin/mtr
-sudo chown root $mtrlocation/sbin/mtr
-
-# sort "%!sort -k2nr"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Install other useful binaries
+#
+# sort "%!sort -k2nr"
 brew install ack
 brew install cmatrix
 brew install docker-clean
@@ -51,35 +42,56 @@ brew install dockutil
 brew install fzf
 brew install git
 brew install gnupg2
+brew install go
+brew install hub
 brew install jq
 brew install md5sha1sum
 brew install mongodb
+brew install neovim
 brew install pv
 brew install python
 brew install rename
-brew install task --with-gnutls
-brew install testdisk
 brew install the_silver_searcher
 brew install tmux
 brew install tree
 brew install unrar
 brew install watch
+brew install wget
 brew install youtube-dl
 brew install zopfli
 brew install zsh
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Move to post-install 'cause deez guys super-slow
-# brew install gpg
 # brew install imagemagick --with-webp
 # brew install ffmpeg --with-libvpx
 
-# # Ruby
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Ruby
 # brew install rbenv ruby-build
 
-# Node and NPM
-# https://gist.github.com/DanHerbert/9520689#gistcomment-2129349
-brew install node
-npm install npm --global
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Install node
+#
+brew uninstall --force yarn node npm  # remove previously installed node, npm, yarn
+brew cleanup  # clean all broken symlinks
+brew update  # always good to have the latest
+
+# install Yarn w/o the node dependency
+# https://github.com/yarnpkg/website/blob/13e95d80282f028ed7b28a822818ce128ea70b7e/lang/en/docs/_installations/mac.md
+brew install yarn --ignore-dependencies  # the option --without-node doesn't seem to work anymore @ Feb 2019
+
+# install current 0.34.0 version (Feb 2019)
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+# install a node latest lts (currently dubnium, v10.x, on Feb 2019)
+nvm install lts/dubnium
+
+echo "lts/dubnium" > .nvmrc # to default to the latest LTS version
+nvm alias default lts/dubnium
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Remove outdated versions from the cellar
+#
 brew cleanup
