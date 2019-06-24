1c36d14d4d53c5009325c6515eca3c9d34e7fc12
Mike Hume <mhumesf@gmail.com>
2019-06-24 10:54:05 -0700

    Symlinks and folding gitconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,13 @@
 ./osx.sh
 ./npm-setup.sh
 
+# symlink config files
+.gitconfig
+.gitignore
+.zshrc
+mkdir -p $HOME/.config/nvim
+ln -s $HOME/dotfiles/.config/nvim/init.vim
+
 # Vim Setup
 cd $HOME
 curl -L https://raw.github.com/zaiste/vimified/master/install.sh | sh
