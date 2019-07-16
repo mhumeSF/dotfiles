cebe8ad44f4abe4ef8f0c5029385768560b149c6
Mike Hume <mhumesf@gmail.com>
2019-07-15 21:21:34 -0700

    $HOME -> ~

--- install_drush.sh
+++
@@ -1,8 +0,0 @@
-curl -O https://getcomposer.org/composer.phar
-mv composer.phar /usr/local/bin/composer
-chmod +x /usr/local/bin/composer
-
-echo 'export PATH="$HOME/.composer/vendor/bin:$PATH"' >> ~/.zshrc
-source ~/.zshrc
-
-composer global require drush/drush
