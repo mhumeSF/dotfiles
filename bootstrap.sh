32cb34b8d015b7a738c4a5c0aa264b3bb2e862ae
Mike Hume <mhumesf@gmail.com>
2021-02-02 23:50:09 -0800

    Drop vimified and cleanup symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,13 +8,13 @@
 
 # symlink config files
 ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-ln -s $HOME/dotfiles/.gitignore $HOME/.gitconfig
+ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/.tmuxcolors.conf $HOME/.tmuxcolors.conf
 mkdir -p $HOME/.config/nvim
-ln -s $HOME/dotfiles/.config/nvim/init.vim
+ln -s $HOME/dotfiles/init.vim ~/.config/nvim/init.vim
 
-# Vim Setup
-cd $HOME
-curl -L https://raw.github.com/zaiste/vimified/master/install.sh | sh
+# # Vim Setup
+# cd $HOME
+# curl -L https://raw.github.com/zaiste/vimified/master/install.sh | sh
