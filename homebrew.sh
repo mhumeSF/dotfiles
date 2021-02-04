4b8460ef439a0477a22eba2d9f8d0f641679b47f
finn <finn@finns-Air.local>
2021-02-03 20:38:57 -0800

    Move files to conf

--- homebrew.sh
+++ homebrew.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-##############################################################################################################
-### homebrew!
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# install all the things
-./brew.sh
-./brew-cask.sh
-
-### end of homebrew
-##############################################################################################################
