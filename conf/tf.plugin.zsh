20df7f9ea7ebcae21e18d6f4ad23c25c9cd9c4d4
Mike Hume <mhumesf@gmail.com>
2021-02-10 18:12:28 -0800

    adding resolve_tfenv_version

--- conf/tf.plugin.zsh
+++ conf/tf.plugin.zsh
@@ -4,6 +4,7 @@ autokey() {
   GIT_FOLDER=$(basename $(git rev-parse --show-toplevel))
   export TF_CLI_ARGS_init="-backend-config=\"key=${GIT_FOLDER}${PWD##${GIT_ROOT}}/terraform.tfstate\""
 }
+
 tfenv() {
     if [[ $1 == "use" ]]; then
         echo $2 > $HOME/.terraform-version
@@ -12,6 +13,7 @@ tfenv() {
         command tfenv "$@"
     fi
 }
+
 # alias terraforming='docker run --rm \
 #   --name terraforming \
 #   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
@@ -21,3 +23,28 @@ tfenv() {
 #   -e AWS_SECURITY_TOKEN=$AWS_SECURITY_TOKEN \
 #   quay.io/dtan4/terraforming:latest \
 #   terraforming'
+
+function resolve_tfenv_version() {
+  local VERSION_FILE=".terraform-version"
+
+  if [ ! -z $TFENV_TERRAFORM_VERSION];
+  then
+    VERSION=$TFENV_TERRAFORM_VERSION
+
+  elif [ -f "./$VERSION_FILE" ];
+  then
+    VERSION=$(cat "./$VERSION_FILE")
+
+  elif [ -f "$(git rev-parse --show-toplevel)/$VERSION_FILE" ];
+  then
+    VERSION=$(cat "$(git rev-parse --show-toplevel)/$VERSION_FILE");
+
+  elif [ -f "$HOME/$VERSION_FILE" ];
+  then
+    VERSION=$(cat $HOME/$VERSION_FILE)
+  else
+    echo "null"
+  fi
+
+  echo $VERSION
+}
