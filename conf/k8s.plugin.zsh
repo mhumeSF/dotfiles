cd77ad72ac95159c76a7a3e131d1ddfbf3fe7427
Mike Hume <mhumesf@gmail.com>
2022-02-17 00:20:57 -0800

    kubectl aliases

--- conf/k8s.plugin.zsh
+++ conf/k8s.plugin.zsh
@@ -1,3 +1,7 @@
+alias k="kubectl"
+alias kc="kubectl-ctx"
+alias kn="kubectl-ns"
+
 function kubectl() {
   if ! type __start_kubectl >/dev/null 2>&1; then
     source <(command kubectl completion zsh)
