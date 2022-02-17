f0c79f25b4c1aaf7052087f0631bbeb5b89b009f
Mike Hume <mhumesf@gmail.com>
2022-02-17 00:20:16 -0800

    Bump python version

--- _python.sh
+++ _python.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pyenv install 3.9.1
-pyenv global 3.9.1
+pyenv install 3.10.2
+pyenv global 3.10.2
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
