6250ff39c96c2c19038ed5f0bd0970068395a90f
Mike Hume <1088457+mhumeSF@users.noreply.github.com>
2025-12-05 13:21:38 -0500

    Update install instructions

--- install.sh
+++ install.sh
@@ -9,31 +9,26 @@ brew install Alacritty --cask
 xattr -d com.apple.quarantine /Applications/Alacritty.app
 
 # Open Security & Privacy preferences
+# Explain why this is required for terminal to write to defaults
 open "x-apple.systempreferences:com.apple.preference.security?Privacy_AllFiles"
 
 # Wait for user input
 read -p "Press Enter to continue"
 
-# Install recommended vanilla using determinate.systems install script as
-# perscribed by nix-darwin. Say no when prompted to install Determinate Nix.
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+# Use --prefer-upstream-nix for vanilla linux
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --prefer-upstream-nix
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-
-sudo nix-channel --add https://channels.nixos.org/nixpkgs-unstable nixpkgs-unstable
-sudo nix-channel --add https://channels.nixos.org/nixpkgs-24.11-darwin nixpkgs
-sudo nix-channel --update
 
 # Clone dotfiles to home
 nix-shell -p git --run "git clone https://github.com/mhumesf/nix-dotfiles $HOME/dotfiles"
 
 # Use nix to invoke nix-darwin to install dotfiles
-sudo nix -i run --extra-experimental-features "nix-command flakes" nix-darwin/nix-darwin-25.05#darwin-rebuild -- switch --flake  ~/dotfiles/ --impure
+sudo -i nix run --extra-experimental-features "nix-command flakes" nix-darwin/nix-darwin-25.05#darwin-rebuild -- switch --flake  ~/dotfiles/ --impure
 
-# Uninstall nix-darwin
+# UNINSTALL NIX-DARWIN
 # nix --extra-experimental-features "nix-command flakes" run nix-darwin#darwin-uninstaller
 
-# Uninstall nix
+# UNINSTALL NIX
 # /nix/nix-installer uninstall
-
-rm -rf "/Users/${USER}/.local/state/nix/profiles/home-manager*"
-rm -rf "/Users/${USER}/.local/state/home-manager/gcroots/current-home"
+# rm -rf "/Users/${USER}/.local/state/nix/profiles/home-manager*"
+# rm -rf "/Users/${USER}/.local/state/home-manager/gcroots/current-home"
