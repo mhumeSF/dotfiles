82f95d209408b970a3c97d9b66f580c13f8f4514
Mike Hume <1088457+mhumeSF@users.noreply.github.com>
2025-12-12 14:08:20 -0500

    flake update

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -43,11 +43,11 @@
     caskArgs.no_quarantine = true;
     global.brewfile = true;
     # mas search <app> to get "<app> = xxx"
-    # masApps = {
-    #   "1Password for Safari" = 1569813296;
-    #   "Divvy" = 413857545;
-    #   "WireGuard" = 1451685025;
-    # };
+    masApps = {
+      "1Password for Safari" = 1569813296;
+      "Divvy" = 413857545;
+      "WireGuard" = 1451685025;
+    };
     brews = [
       "dockutil"
       "tfenv"
