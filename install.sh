e1327433ba882c9fb9acb82f6acf2d53dc0c3713
Mike Hume <1088457+mhumeSF@users.noreply.github.com>
2025-12-12 14:20:22 -0500

    Update install instructions

--- install.sh
+++ install.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
+set -e
 
-# Install Homebrew
+echo "=== Installing Homebrew ==="
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install Alacritty --cask
-xattr -d com.apple.quarantine /Applications/Alacritty.app
-
-# Open Security & Privacy preferences
-# Explain why this is required for terminal to write to defaults
+echo "=== Requesting Full Disk Access ==="
+echo "Please grant Full Disk Access to your terminal in the Security & Privacy preferences."
 open "x-apple.systempreferences:com.apple.preference.security?Privacy_AllFiles"
+read -p "Press Enter after granting permissions..."
 
-# Wait for user input
-read -p "Press Enter to continue"
-
+echo "=== Installing Nix ==="
 # Use --prefer-upstream-nix for vanilla linux
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
-# Clone dotfiles to home
+echo "=== Cloning dotfiles ==="
 nix-shell -p git --run "git clone https://github.com/mhumesf/nix-dotfiles $HOME/dotfiles"
 
-# Use nix to invoke nix-darwin to install dotfiles
-sudo -i nix run --extra-experimental-features "nix-command flakes" nix-darwin/nix-darwin-25.11#darwin-rebuild -- switch --flake  ~/dotfiles/ --impure
+echo "=== Applying nix-darwin configuration ==="
+sudo -i nix run --extra-experimental-features "nix-command flakes" nix-darwin/nix-darwin-25.11#darwin-rebuild -- switch --flake ~/dotfiles/ --impure
 
-# UNINSTALL NIX-DARWIN
-# nix --extra-experimental-features "nix-command flakes" run nix-darwin#darwin-uninstaller
+echo "=== Installation complete! ==="
 
-# UNINSTALL NIX
-# /nix/nix-installer uninstall
-# rm -rf "/Users/${USER}/.local/state/nix/profiles/home-manager*"
-# rm -rf "/Users/${USER}/.local/state/home-manager/gcroots/current-home"
+# UNINSTALL COMMANDS (commented out):
+# nix-darwin: nix --extra-experimental-features "nix-command flakes" run nix-darwin#darwin-uninstaller
+# nix: /nix/nix-installer uninstall
+# home-manager cleanup: rm -rf "/Users/${USER}/.local/state/nix/profiles/home-manager*" "/Users/${USER}/.local/state/home-manager/gcroots/current-home"
