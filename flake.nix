1280b2079b96e8d4ec033d08d9f619d39ab18b54
Mike Hume <1088457+mhumeSF@users.noreply.github.com>
2026-06-10 09:04:41 -0400

    Bump nixpkgs, nix-darwin, home-manager to 26.05
    Brings in docker_29 as the default, replacing the now-unmaintained
    docker_28 that was flagged insecure in 25.11.

--- flake.nix
+++ flake.nix
@@ -3,16 +3,16 @@
 
   inputs = {
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     darwin = {
-      url = "github:LnL7/nix-darwin/nix-darwin-25.11";
+      url = "github:LnL7/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
